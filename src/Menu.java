3ccc3a6598cfccc8cc5872eb462f5e8cdb647418
Risa <risaiwazaki>
2022-04-05 09:52:18 -0700

    menu ideas

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,7 @@
 public class Menu {
 }
+
+
+//have a dict that connects to input, to a thread of every challenge, threads would be created outside the menu, where we could refer to them, menu = input the value, search through the dict of inputs of threads, run that thread for the start
+//take input, dict, etc.
+
